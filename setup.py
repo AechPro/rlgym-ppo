6052e5c5720b1d3fdfafeb2c916a4f97e5073264
FlamingFury00 <manuel.digiovanni13.mdg@gmail.com>
2024-03-01 02:12:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 
 
-__version__ = "1.3.5"
+__version__ = "1.3.6"
 
 with open('README.md', 'r') as readme_file:
     long_description = readme_file.read()
